916dcad000bbde9f9c939f5389039c9b7bf6e9c1
yejeii <98299007+yejeii@users.noreply.github.com>
2024-07-02 16:37:51 +0900

    2024.07.02 2th commit
    node.js 설치 후 테스트

---
+++ javascript/ch03/circle.js
@@ -0,0 +1,8 @@
+const PI = 3.141592;
+function getArea(radius) {
+  return PI * radius;
+}
+function getCircumgerence(radius) {
+  return 2 * PI * radius;
+}
+export { PI, getArea, getCircumgerence };
---
+++ javascript/ch03/index.js
@@ -0,0 +1,4 @@
+import * as circle from "./circle.js";
+
+console.log(circle.PI, circle.getArea(1), circle.getCircumgerence(1));
+//console.log("index run");
