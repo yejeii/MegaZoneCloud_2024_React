214839db8d467463238a5893c895b8422ece6a48
yejeii <98299007+yejeii@users.noreply.github.com>
2024-07-04 12:51:53 +0900

    ignore file&folder config

--- javascript/ch03/circle.js
+++
@@ -1,8 +0,0 @@
-const PI = 3.141592;
-function getArea(radius) {
-  return PI * radius;
-}
-function getCircumgerence(radius) {
-  return 2 * PI * radius;
-}
-export { PI, getArea, getCircumgerence };
